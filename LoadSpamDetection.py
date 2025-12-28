e10b2b18a1ccacddc2a6b1e014b40ed82c24b582
Kaushal Jha <jhakaushal.1809@gmail.com>
2025-12-28 11:05:19 +0530

    Add files via upload

---
+++ LoadSpamDetection.py
@@ -0,0 +1,25 @@
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+#Load Model
+model = joblib.load('D:\Programs\Python\Projects\spamDetection.pkl')
+
+#User Input
+num_link = int(input("Enter number of links: "))
+num_words = int(input("Enter number of words: "))
+has_offer = int(input("Enter has offer (0 or 1): "))
+sender_score = float(input("Enter sender score: "))
+all_caps = int(input("Enter all caps (0 or 1): "))
+
+new_email = [num_link, num_words, has_offer, sender_score, all_caps]
+
+scaler = StandardScaler()
+new_email_transform = scaler.fit_transform([new_email])
+
+#Prediction
+prediction = model.predict(new_email_transform)
+
+if prediction == 0:
+    print("Email is not spam")
+elif prediction == 1:
+    print("Email is spam")
---
+++ SpamDetection.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler
+import joblib
+
+#Load dataset
+data = pd.read_csv("D:\Programs\Python\Projects\spam_detection_dataset.csv")
+
+#Separate X and y
+X = data.drop('is_spam', axis=1)
+y = data['is_spam']
+
+#Scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+#Split dataset
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+#Train Logistic Regression Model
+model = LogisticRegression(class_weight= {0:1, 1:3})
+model.fit(X_train, y_train)
+
+#Prediction
+pred = model.predict(X_test)
+
+#Evaluation
+print("Accuracy: ", accuracy_score(y_test, pred))
+print("Confusion Matrix: ", confusion_matrix(y_test, pred))
+print("Classification report :", classification_report(y_test, pred))
+
+#Save Model
+joblib.dump(model, 'spamDetection.pkl')
